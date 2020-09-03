a93e5aa5ff68d242617a252a53237a5a01e21b23
wismaeka7 <67813045+wismaeka@users.noreply.github.com>
2020-09-04 00:17:52 +0800

    - add api movie using themoviedb - add router GET movies/:genre - add router POST movies - add static showMovies and searchMovies on MovieController

--- server/router/movie.js
+++ server/router/movie.js
@@ -1,6 +1,7 @@
 const route = require('express').Router()
 const MovieController = require('../controllers/MovieController')
 
-route.get('/', MovieController.showMovies)
+route.get('/:genre', MovieController.showMovies)
+route.post('/', MovieController.searchMovies)
 
 module.exports = route
